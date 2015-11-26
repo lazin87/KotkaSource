5ca0302c3c1a8127ad649a294d84be48319f1910
lazin87 <rafal.lazinski@gmail.com>
2015-11-26 16:00:34 +0100

    impl of read write method from isource

--- isource.cpp
+++ isource.cpp
@@ -2,8 +2,10 @@
 
 #include "isourcetranslatestrategy.h"
 
-ISource::ISource()
+ISource::ISource(QString const & a_rFileName)
     : m_pTranslateStrategy(0)
+    , m_fIsReadOnly(true)
+    , m_oFile(a_rFileName)
 {
 
 }
@@ -16,5 +18,47 @@ void ISource::setStrategy(ISourceTranslateStrategy *a_pTranslateStrategy)
 ISourceTranslateStrategy *ISource::strategy() const
 {
     return m_pTranslateStrategy;
+}
+
+void ISource::setReadOnlyFlag(bool a_fIsReadOnly)
+{
+    m_fIsReadOnly = a_fIsReadOnly;
+}
+
+bool ISource::readTaskData(QList<KotkaSource::STaskData> a_rOutTaskList)
+{
+    bool fResult = false;
+
+    if( (0 != m_pTranslateStrategy) && m_oFile.exists() )
+    {
+        fResult = m_oFile.open(QIODevice::ReadOnly | QIODevice::Text);
+        if(fResult)
+        {
+            QTextStream inTextStream(&m_oFile);
+            fResult = m_pTranslateStrategy->getTaskDataList(a_rOutTaskList, inTextStream);
+            m_oFile.close();
+        }
+    }
+
+    return fResult;
+}
+
+bool ISource::storeTaskData(const KotkaSource::STaskData &a_crTaskData)
+{
+    bool fResult = false;
+
+    if( (false == m_fIsReadOnly) && (0 != m_pTranslateStrategy) && m_oFile.exists() )
+    {
+        fResult = m_oFile.open(QIODevice::ReadWrite | QIODevice::Text);
+        if(fResult)
+        {
+            QTextStream inOutTextStream(&m_oFile);
+            fResult = m_pTranslateStrategy->fillInSourceDoc(a_crTaskData, inOutTextStream);
+            inOutTextStream.flush();
+            m_oFile.close();
+        }
+    }
+
+    return fResult;
 }
 
--- isource.h
+++ isource.h
@@ -1,18 +1,29 @@
 #ifndef ISOURCE_H
 #define ISOURCE_H
 
+#include <QFile>
+#include <QString>
+
+#include "commonddefs.h"
+
 class ISourceTranslateStrategy;
 
 class ISource
 {
 public:
-    ISource();
+    explicit ISource(QString const & a_rFileName);
 
     void setStrategy(ISourceTranslateStrategy * a_pTranslateStrategy);
     ISourceTranslateStrategy * strategy() const;
 
+    void setReadOnlyFlag(bool a_fIsReadOnly);
+    bool readTaskData(QList<KotkaSource::STaskData> a_rOutTaskList);
+    bool storeTaskData(KotkaSource::STaskData const & a_crTaskData);
+
 private:
     ISourceTranslateStrategy * m_pTranslateStrategy;
+    bool m_fIsReadOnly;
+    QFile m_oFile;
 };
 
 #endif // ISOURCE_H
